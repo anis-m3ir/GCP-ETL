cab8828ea25d035292f6506bdb7e58854802c8ba
anis-m3ir <anisguellal3@gmail.com>
2026-03-14 03:28:46 +0100

    feat: init retail etl gcp project

---
+++ infra/main.tf
@@ -0,0 +1,212 @@
+# GCS Bucket - stockage des fichiers CSV
+resource "google_storage_bucket" "bucket" {
+  name          = "retail-etl-dga"
+  location      = "EU"
+  uniform_bucket_level_access = true 
+  force_destroy = true
+}
+
+
+# BigQuery Dataset
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id    = "retail_dga"
+  friendly_name = "Retail Dataset"
+  description   = "Retail Dataset"
+  location      = "EU"
+}
+
+# terraform to create table in bigquery
+resource "google_bigquery_table" "raw_country" {
+  dataset_id = "retail_dga"
+  table_id   = "raw_country"
+  schema     = <<EOF
+  [
+    {
+      "name": "id",
+      "type": "STRING"
+    },
+    {
+      "name": "iso",
+      "type": "STRING"
+    },
+    {
+      "name": "name",
+      "type": "STRING"
+    },
+    {
+      "name": "nicename",
+      "type": "STRING"
+    },
+    {
+      "name": "iso3",
+      "type": "STRING"
+    },
+    {
+      "name": "numcode",
+      "type": "STRING"
+    },
+    {
+      "name": "phonecode",
+      "type": "STRING"
+    }]
+  EOF
+}
+# terraform to create table raw_invoice  InvoiceNo,StockCode,Description,Quantity,InvoiceDate,UnitPrice,CustomerID,Country
+resource "google_bigquery_table" "raw_invoice" {
+  dataset_id = "retail_dga"
+  table_id   = "raw_invoice"
+  schema     = <<EOF
+  [
+    {
+      "name": "InvoiceNo",
+      "type": "STRING"
+    },
+    {
+      "name": "StockCode",
+      "type": "STRING"
+    },
+    {
+      "name": "Description",
+      "type": "STRING"
+    },
+    {
+      "name": "Quantity",
+      "type": "STRING"
+    },
+    {
+      "name": "InvoiceDate",
+      "type": "STRING"
+    },
+    {
+      "name": "UnitPrice",
+      "type": "STRING"
+    },
+    {
+      "name": "CustomerID",
+      "type": "STRING"
+    },
+    {
+      "name": "Country",
+      "type": "STRING"
+    }]
+  EOF
+}
+
+#WORKFLOOOOOOW
+resource "google_workflows_workflow" "workflow" {
+  name            = "retail-dga-workflow"
+  description     = "Retail Dataset Workflow"
+  source_contents = local.workflow_yaml
+  region          = var.region
+  service_account = google_service_account.service_account.email
+}
+
+# terraform to enable Identity and Access Management (IAM) API
+resource "google_project_service" "service" {
+  service = "iam.googleapis.com"
+}
+
+# terraform to create service account
+resource "google_service_account" "service_account" {
+  account_id   = "retail-etl-sa"
+  display_name = "Retail ETL SA"
+}
+
+# terraform to enable Cloud Run API
+resource "google_project_service" "cloud_run_api" {
+  service = "run.googleapis.com"
+}
+
+# terraform to enable Artifact Registry API
+resource "google_project_service" "artifact_registry_api" {
+  service = "artifactregistry.googleapis.com"
+}
+
+# terraform to create Artifact Registry Docker repository for dbt images
+resource "google_artifact_registry_repository" "dbt_images" {
+  location      = var.region
+  repository_id = var.ar_repo_name
+  description   = "dbt images"
+  format        = "DOCKER"
+}
+
+# terraform to grant roles gcs reader to service account
+resource "google_project_iam_member" "storage_object_viewer" {
+  project = var.project_id
+  role    = "roles/storage.objectViewer"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+# terraform to grant roles workflow invoker to service account
+resource "google_project_iam_member" "workflow_invoker" {
+  project = var.project_id
+  role    = "roles/workflows.invoker"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+# terraform to grant roles eventarc admin to service account
+resource "google_project_iam_member" "eventarc_admin" {
+  project = var.project_id
+  role    = "roles/eventarc.admin"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+# terraform to grant roles eventarc admin to service account
+resource "google_project_iam_member" "eventarc_storage_check" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+# terraform to create Cloud Run Job to execute dbt with BigQuery
+resource "google_cloud_run_v2_job" "dbt" {
+  name     = var.dbt_job_name
+  location = var.region
+
+  template {
+    template {
+      service_account = google_service_account.service_account.email
+      max_retries     = 1
+      timeout         = "1800s"
+
+      containers {
+        image = var.dbt_image
+        args  = ["run"]
+      }
+    }
+  }
+}
+
+# terraform to grant Cloud Run Job runner to service account
+resource "google_cloud_run_v2_job_iam_member" "run_job_runner" {
+  name     = google_cloud_run_v2_job.dbt.name
+  location = google_cloud_run_v2_job.dbt.location
+  role     = "roles/run.invoker"
+  member   = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+# terraform to grant Cloud Run developer role to service account
+resource "google_project_iam_member" "run_developer" {
+  project = var.project_id
+  role    = "roles/run.developer"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+# EVENTARC
+resource "google_eventarc_trigger" "trigger" {
+  name            = "retail-dga-trigger"
+  location        = "eu"
+  service_account = google_service_account.service_account.email
+  matching_criteria {
+    attribute = "type"
+    value     = "google.cloud.storage.object.v1.finalized"
+  }
+  matching_criteria {
+    attribute = "bucket"
+    value     = google_storage_bucket.bucket.name
+  }
+
+  destination {
+    workflow = google_workflows_workflow.workflow.id
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,52 @@
+variable "project_id" {
+  type        = string
+  default     = "retail-etl-489114"
+  description = "The project id"
+}
+
+variable "region" {
+  type        = string
+  default     = "europe-west1"
+  description = "Region for regional GCP resources"
+}
+
+variable "dbt_job_name" {
+  type        = string
+  default     = "retail-etl-dbt-job"
+  description = "Cloud Run job name that executes dbt"
+}
+
+variable "dbt_image" {
+  type        = string
+  default     = "docker.io/dbt-labs/dbt-bigquery:1.8.2"
+  description = "Container image used by Cloud Run Job to run dbt with BigQuery"
+}
+
+
+variable "tf_state_bucket" {
+  type        = string
+  default     = "retail-etl-tfstate-ag"
+  description = "GCS bucket used by Terraform remote state"
+}
+
+variable "tf_state_prefix" {
+  type        = string
+  default     = "terraform/infra"
+  description = "Prefix/path used in GCS backend for Terraform state"
+}
+
+variable "ar_repo_name" {
+  type        = string
+  default     = "dbt-images"
+  description = "Artifact Registry Docker repo for dbt images"
+}
+
+variable "github_owner" {
+    type = string
+        description = "Ganis-m3ir"
+}
+
+variable "github_repo_name" {
+    type = string
+        description = "GCP-ETL"
+}
